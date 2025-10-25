62fa0ac57ee02b4e603646a9c71b07ebbf282890
ChoonXiangg <119977822+ChoonXiangg@users.noreply.github.com>
2025-10-25 16:31:21 +0000

    Added code for debugging

--- app/database.py
+++ app/database.py
@@ -3,5 +3,5 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-engine = create_engine(DATABASE_URL, connect_args=connect_args)
+engine = create_engine(DATABASE_URL, connect_args=connect_args, echo=True) #echo=True prints SQL + params
 SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
